0afadda20b5e4b87fae457c79824779f5c560ef5
MIS Productions <maqbul.yusuf@sky.com>
2021-10-12 15:54:25 +0100

    Update Widget.js

--- Widget.js
+++ Widget.js
@@ -13,7 +13,7 @@
   To check for updates or to leave feedback, tap on widget
   ##########################################################
 */
-var test="mis"
+//var test="mis"
 
 async function main() {
 
