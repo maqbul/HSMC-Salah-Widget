a87ab59d1deb9ba87c7e6c93fbb6af1bec431f31
MIS Productions <maqbul.yusuf@sky.com>
2021-10-14 14:44:29 +0100

    Update Main_Wrapper.js

--- Main_Wrapper.js
+++ Main_Wrapper.js
@@ -4,7 +4,7 @@
   Salah Widget v1.0 
   Developed by: Maqbul Yusuf
   Email: maqbul.yusuf@sky.com
-  Date: 12/10/21
+  Date: 14/10/21
   Compatible with iOS (scriptable app)
      
   Please do NOT remove or modify this header
