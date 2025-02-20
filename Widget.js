88e66e4446ec7a4848d3b4cd9efc86e2199c873e
MIS Productions <maqbul.yusuf@sky.com>
2025-02-20 10:09:56 +0000

    Update Widget.js

--- Widget.js
+++ Widget.js
@@ -226,7 +226,7 @@ else if(timenow>=maghribb){
 widget.addSpacer(4)
 
 widget.addStack()
-var feedback = widget.addText('  LIKE THE WIDGET?     PLEASE RATE IT - TAP HERE!')
+var feedback = widget.addText(' ')
 feedback.font = Font.headline()
 feedback.font = Font.lightSystemFont(10); 
 feedback.textOpacity=0 
