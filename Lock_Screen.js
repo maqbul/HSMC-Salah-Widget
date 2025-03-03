fdca2c585e1db769f42ac1c74d4efc07409ad209
MIS Productions <maqbul.yusuf@sky.com>
2025-03-03 17:26:54 +0000

    Update Lock_Screen.js

--- Lock_Screen.js
+++ Lock_Screen.js
@@ -62,7 +62,7 @@ var now = new Date();
 var start = new Date(now.getFullYear(), 0, 0);
 var diff = now - start;
 var oneDay = 1000 * 60 * 60 * 24;
-var daynumber = Math.floor(diff / oneDay)-1;
+var daynumber = Math.floor(diff / oneDay); //-1 or +1 if timing is offset a day or so
 
 console.log('Day number: ' + daynumber);
 
